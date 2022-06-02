e2adca01414030ac0b670caf73558599e7b5c63b
Ilya Pasternak <pasterzoom@gmail.com>
2022-06-02 18:05:50 +0300

    connect Redux

--- src/state/todolistsReducer.ts
+++ src/state/todolistsReducer.ts
@@ -1,4 +1,4 @@
-import {FilterType, TodolistType} from '../App';
+import {FilterType,  TodolistType} from '../App';
 import {v1} from 'uuid';
 
 export type RemoveTodoListAT = ReturnType<typeof removeTodoListAC>
